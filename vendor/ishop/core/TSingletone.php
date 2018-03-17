535a2b01857490ca4d13ea0a77744b69190fd5ff
pavel02 <pavel.hr@mail.ru>
2018-03-17 23:52:38 +0300

    Трейт для Синглтона

---
+++ vendor/ishop/core/TSingletone.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ishop;
+
+trait TSingletone
+{
+    private static $instance;
+
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new self;
+        }
+
+        return self::$instance;
+    }
+}
